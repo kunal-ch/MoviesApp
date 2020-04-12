a21f2ac77a5a47cd9b2098d8c61cc9b3bb658e6a
kunal chauhan <kunalch36@gmail.com>
2020-04-13 00:10:25 +0530

    - Error Handling

--- app/src/main/java/com/kc/movies/ui/list/MovieListActivity.kt
+++ app/src/main/java/com/kc/movies/ui/list/MovieListActivity.kt
@@ -1,8 +1,10 @@
 package com.kc.movies.ui.list
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.ProgressBar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.Observer
@@ -11,58 +13,92 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kc.movies.R
 import com.kc.movies.model.Movie
+import com.kc.movies.ui.detail.MovieDetailActivity
 import com.kc.movies.utils.Key
 import com.kc.movies.utils.hideKeyboard
-import kotlinx.android.synthetic.main.activity_movielist.*
+import com.kc.movies.utils.isOnline
+import kotlinx.android.synthetic.main.activity_movie_list.*
 
 private const val SPAN_COUNT = 2
 
 class MovieListActivity : AppCompatActivity(), MovieListAdapter.OnInteractionListener {
 
   private lateinit var viewModel: MovieListViewModel
-  private var homeRv: RecyclerView? = null
-  private var homeToolbar: Toolbar? = null
-  private var progressBar: ProgressBar? = null
   private var movieListAdapter: MovieListAdapter? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_movielist)
-    homeRv = findViewById(R.id.homeRv)
-    homeToolbar = findViewById(R.id.homeToolbar)
-    homeToolbar?.title = ""
+    setContentView(R.layout.activity_movie_list)
+
     movieListAdapter = MovieListAdapter(layoutInflater, this)
-    homeRv?.setHasFixedSize(true)
-    homeRv?.layoutManager = GridLayoutManager(this, SPAN_COUNT)
-    homeRv?.adapter = movieListAdapter
-    homeRv?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-      override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-        hideKeyboard(this@MovieListActivity)
-        super.onScrolled(recyclerView, dx, dy)
-      }
-    })
-
-    //hide search keyboard when scrolling
-    homeRv?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
-      override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-        hideKeyboard(this@MovieListActivity)
-        super.onScrollStateChanged(recyclerView, newState)
-      }
-    })
-
     viewModel = ViewModelProviders.of(this).get(MovieListViewModel::class.java)
+    setupUI()
 
     viewModel.moviesLiveData.observe(this, Observer { it ->
       homePb?.visibility = View.GONE
       movieListAdapter?.setMovies(it)
       homeRv?.scheduleLayoutAnimation()
     })
+    checkForConnection()
+    checkForError()
+  }
 
-    viewModel.getMovies()
+  private fun checkForError() {
+    viewModel.errorLiveData.observe(this, Observer { it ->
+      homePb.visibility = View.GONE
+      val builder = AlertDialog.Builder(this)
+      builder.setTitle("MoviesApp")
+      builder.setMessage(it)
+      builder.setIcon(android.R.drawable.ic_dialog_alert)
+      builder.setPositiveButton("OK"){dialogInterface, which ->
+      }
+      val alertDialog: AlertDialog = builder.create()
+      alertDialog.setCancelable(false)
+      alertDialog.show()
+    })
+  }
+
+  private fun setupUI(){
+    setSupportActionBar(homeToolbar)
+    homeToolbar?.title = ""
+
+    homeRv.setHasFixedSize(true)
+    homeRv.layoutManager = GridLayoutManager(this, SPAN_COUNT)
+    homeRv.adapter = movieListAdapter
+
+    //hide search keyboard when scrolling
+    homeRv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+      override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+        hideKeyboard(this@MovieListActivity)
+        super.onScrollStateChanged(recyclerView, newState)
+      }
+    })
+  }
+
+  private fun checkForConnection(){
+    if (!isOnline(this)) {
+      homePb.visibility = View.GONE
+      val builder = AlertDialog.Builder(this)
+      builder.setTitle("MoviesApp")
+      builder.setMessage("No internet connection available!!")
+      builder.setIcon(android.R.drawable.ic_dialog_alert)
+      builder.setPositiveButton("Try Again"){dialogInterface, which ->
+        checkForConnection()
+      }
+      val alertDialog: AlertDialog = builder.create()
+      alertDialog.setCancelable(false)
+      alertDialog.show()
+    } else {
+      homePb.visibility = View.VISIBLE
+      viewModel.getMovies()
+    }
   }
 
   override fun onItemClicked(movie: Movie) {
     val bundle = Bundle()
     bundle.putSerializable(Key.MOVIE, movie)
+    val intent = Intent(this, MovieDetailActivity::class.java)
+    intent.putExtras(bundle)
+    startActivity(intent)
   }
 }
