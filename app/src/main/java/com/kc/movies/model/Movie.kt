c555f7cb0b8abbbdfdf795054863e37f3cd7f547
kunal chauhan <kunalch36@gmail.com>
2020-04-24 16:54:53 +0530

    - Movie Entity

--- app/src/main/java/com/kc/movies/model/Movie.kt
+++ app/src/main/java/com/kc/movies/model/Movie.kt
@@ -1,26 +1,25 @@
 package com.kc.movies.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 import com.kc.movies.model.pages.CreditsPage
 import com.kc.movies.model.pages.VideosPage
 import java.io.Serializable
 
+@Entity(tableName = "movie")
 data class Movie(
-        val id: Long? = null,
-        @SerializedName("vote_count")
-        val voteCount: Int? = null,
-        @SerializedName("vote_average")
-        val voteAverage: Double? = null,
-        val runtime: Int? = null,
-        val title: String? = null,
-        @SerializedName("release_date")
-        val releaseDate: String? = null,
-        @SerializedName("poster_path")
-        val posterPath: String? = null,
-        @SerializedName("backdrop_path")
-        val backdropPath: String? = null,
+        @ColumnInfo(name="id") @PrimaryKey val id: Long? = null,
+        @ColumnInfo(name="voteCount") @SerializedName("vote_count") val voteCount: Int? = null,
+        @ColumnInfo(name="voteAverage") @SerializedName("vote_average") val voteAverage: Double? = null,
+        @ColumnInfo(name="runtime") val runtime: Int? = null,
+        @ColumnInfo(name="title") val title: String? = null,
+        @ColumnInfo(name="releaseDate") @SerializedName("release_date") val releaseDate: String? = null,
+        @ColumnInfo(name="posterPath") @SerializedName("poster_path") val posterPath: String? = null,
+        @ColumnInfo(name="backdropPath") @SerializedName("backdrop_path") val backdropPath: String? = null,
         val genres: List<Genre>? = null,
-        val overview: String? = null,
+        @ColumnInfo(name="overview") val overview: String? = null,
         val credits: CreditsPage? = null,
         val videos: VideosPage? = null
 ) : Serializable
